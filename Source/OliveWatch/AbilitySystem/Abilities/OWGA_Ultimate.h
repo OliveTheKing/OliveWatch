9b8455b53000f45a8b30d13c2e7a07c897fa0d3f
syeon <syeon.dev@gmail.com>
2025-05-05 15:21:46 +0900

    Fix: GameAbility의 세부 역할 분리

---
+++ Source/OliveWatch/AbilitySystem/Abilities/OWGA_Ultimate.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/OWGameplayAbility.h"
+#include "OWGA_Ultimate.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OLIVEWATCH_API UOWGA_Ultimate : public UOWGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
