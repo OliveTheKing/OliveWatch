32002599b3bc99f0cc640e8c40b0151371bf2e77
syeon <syeon.dev@gmail.com>
2025-05-05 16:40:49 +0900

    Feat: 궁극기 스킬 코스트 구현

--- Source/OliveWatch/AbilitySystem/Abilities/OWGA_Ultimate.h
+++ Source/OliveWatch/AbilitySystem/Abilities/OWGA_Ultimate.h
@@ -7,11 +7,24 @@
 #include "OWGA_Ultimate.generated.h"
 
 /**
- * 
+ * 궁극기 스킬입니다.
  */
 UCLASS()
 class OLIVEWATCH_API UOWGA_Ultimate : public UOWGameplayAbility
 {
 	GENERATED_BODY()
-	
+
+public:
+	UOWGA_Ultimate();
+
+	virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	/**
+	* 궁극기 사용에 필요한 코스트
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Costs")
+	FScalableFloat UltCost;
 };
