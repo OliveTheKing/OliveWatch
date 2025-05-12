0c49cd5db96d5d5196aa1821a1f99dcd28a906ae
syeon <syeon.dev@gmail.com>
2025-05-12 21:30:50 +0900

    Feat: 쿨타임, 코스트 책임 이전 후 코스트 로직 변경

--- Source/OliveWatch/Public/AbilitySystem/Abilities/Base/OWGameplayAbility.h
+++ Source/OliveWatch/Public/AbilitySystem/Abilities/Base/OWGameplayAbility.h
@@ -6,6 +6,7 @@
 #include "Abilities/GameplayAbility.h"
 #include "OWGameplayAbility.generated.h"
 
+class UOWGE_Cost;
 /**
  * 
  */
@@ -16,4 +17,35 @@ class OLIVEWATCH_API UOWGameplayAbility : public UGameplayAbility
 
 public:
 	UOWGameplayAbility();
+
+	/**
+	* 쿨타임에 필요한 데이터를 설정합니다.
+	* @param Duration 쿨타임 시간
+	* @param Tags 쿨타임 전 재사용을 제약할 태그(InputTags 사용 중)
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Cooldowns")
+	void SetCooldownData(const FScalableFloat& Duration, const FGameplayTagContainer& Tags);
+
+	/**
+	* 코스트에 필요한 데이터를 설정합니다.
+	* @param SkillCost 코스트 값(양수)
+	* @param Tags 쿨타임 전 재사용을 제약할 태그(InputTags 사용 중)
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Costs")
+	void SetCostEffect(const UOWGE_Cost* CostEffect);
+
+	virtual void ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags, const FGameplayTagContainer* TargetTags, OUT FGameplayTagContainer* OptionalRelevantTags) const override;
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	/**
+	* 쿨타임 사용에 필요한 코스트
+	*/
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Cooldowns")
+	FScalableFloat CooldownDuration;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Cooldowns")
+	FGameplayTagContainer CooldownTags;
 };
