104e6036336b0364af0e08da89ccad6f5c65c9db
Yongmalyang <Yongmalyang@users.noreply.github.com>
2025-05-13 02:28:50 +0000

    deploy: 345756f2c0621a35739c7051550637d0ed46c592

--- search/classes_2.js
+++ search/classes_2.js
@@ -7,11 +7,12 @@ var searchData=
   ['uowcooldowngameplayeffect_4',['UOWCooldownGameplayEffect',['../classUOWCooldownGameplayEffect.html',1,'']]],
   ['uowga_5factive_5',['UOWGA_Active',['../classUOWGA__Active.html',1,'']]],
   ['uowga_5factive_5fgunfire_6',['UOWGA_Active_GunFire',['../classUOWGA__Active__GunFire.html',1,'']]],
-  ['uowga_5fpassive_7',['UOWGA_Passive',['../classUOWGA__Passive.html',1,'']]],
-  ['uowga_5fultimate_8',['UOWGA_Ultimate',['../classUOWGA__Ultimate.html',1,'']]],
-  ['uowgameplayability_9',['UOWGameplayAbility',['../classUOWGameplayAbility.html',1,'']]],
-  ['uowgameplayeffect_10',['UOWGameplayEffect',['../classUOWGameplayEffect.html',1,'']]],
-  ['uowge_5fcost_11',['UOWGE_Cost',['../classUOWGE__Cost.html',1,'']]],
-  ['uowgt_5fplaymontageandwaitforevent_12',['UOWGT_PlayMontageAndWaitForEvent',['../classUOWGT__PlayMontageAndWaitForEvent.html',1,'']]],
-  ['uowinputdata_13',['UOWInputData',['../classUOWInputData.html',1,'']]]
+  ['uowga_5ffastzoom_7',['UOWGA_FastZoom',['../classUOWGA__FastZoom.html',1,'']]],
+  ['uowga_5fpassive_8',['UOWGA_Passive',['../classUOWGA__Passive.html',1,'']]],
+  ['uowga_5fultimate_9',['UOWGA_Ultimate',['../classUOWGA__Ultimate.html',1,'']]],
+  ['uowgameplayability_10',['UOWGameplayAbility',['../classUOWGameplayAbility.html',1,'']]],
+  ['uowgameplayeffect_11',['UOWGameplayEffect',['../classUOWGameplayEffect.html',1,'']]],
+  ['uowge_5fcost_12',['UOWGE_Cost',['../classUOWGE__Cost.html',1,'']]],
+  ['uowgt_5fplaymontageandwaitforevent_13',['UOWGT_PlayMontageAndWaitForEvent',['../classUOWGT__PlayMontageAndWaitForEvent.html',1,'']]],
+  ['uowinputdata_14',['UOWInputData',['../classUOWInputData.html',1,'']]]
 ];
