32304c985767f8ef88df7d3a9ca8a5003a665b78
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-05-13 09:32:34 +0900

    Feat: Fast Zoom

--- Source/OliveWatch/Private/AbilitySystem/Abilities/OWGA_FastZoom.cpp
+++ Source/OliveWatch/Private/AbilitySystem/Abilities/OWGA_FastZoom.cpp
@@ -1,5 +1,32 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "AbilitySystem/Abilities/OWGA_FastZoom.h"
+#include "Camera/PlayerCameraManager.h"
+#include "GameFramework/PlayerController.h"
 
+void UOWGA_FastZoom::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo,
+    const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    APlayerController* PC = Cast<APlayerController>(ActorInfo->PlayerController);
+    if (PC && PC->PlayerCameraManager)
+    {
+        OriginalFOV = PC->PlayerCameraManager->GetFOVAngle();
+        PC->PlayerCameraManager->SetFOV(ZoomFOV);
+    }
+}
+
+void UOWGA_FastZoom::EndAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo,
+    const FGameplayAbilityActivationInfo ActivationInfo,
+    bool bReplicateEndAbility, bool bWasCancelled)
+{
+    APlayerController* PC = Cast<APlayerController>(ActorInfo->PlayerController);
+    if (PC && PC->PlayerCameraManager)
+    {
+        PC->PlayerCameraManager->SetFOV(OriginalFOV);
+    }
+
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
