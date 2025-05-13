d0ffbbfac3d38edb9e20aca901b442856dc77d00
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-05-13 09:23:43 +0900

    Add: Fast Zoom files

---
+++ Source/OliveWatch/Public/AbilitySystem/Abilities/OWGA_FastZoom.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/Base/OWGA_Active.h"
+#include "OWGA_FastZoom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OLIVEWATCH_API UOWGA_FastZoom : public UOWGA_Active
+{
+	GENERATED_BODY()
+	
+};
