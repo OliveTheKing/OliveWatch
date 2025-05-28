7241b90daf21c0ddd1b60b8729e0cbbe7bb9a877
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-05-28 11:52:40 +0900

    Add: CyberAgility cpp files

---
+++ Source/OliveWatch/Public/AbilitySystem/Abilities/OWGA_CyberAgility.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/Base/OWGA_Active.h"
+#include "OWGA_CyberAgility.generated.h"
+
+UCLASS()
+class OLIVEWATCH_API UOWGA_CyberAgility : public UOWGA_Active
+{
+	GENERATED_BODY()
+	
+};
