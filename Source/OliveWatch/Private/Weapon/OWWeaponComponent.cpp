536d7e7b8a07a5123dd85c4fa8e2e0b2b9863214
dubidubob <2076397@ewhain.net>
2025-06-18 14:15:12 +0900

    Feat: Raycast

--- Source/OliveWatch/Private/Weapon/OWWeaponComponent.cpp
+++ Source/OliveWatch/Private/Weapon/OWWeaponComponent.cpp
@@ -1,6 +1,10 @@
-#include "Weapon/OWWeaponComponent.h"
+ï»¿#include "Weapon/OWWeaponComponent.h"
 #include "Weapon/OWFireDataAsset.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemComponent.h"
+#include "Player/OWPlayerState.h"
+#include "OWGameplayTags.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Camera/CameraComponent.h"
 
 // Sets default values for this component's properties
@@ -14,8 +18,9 @@ void UOWWeaponComponent::BeginPlay()
 {
     Super::BeginPlay();
     CachedOwningCharacter = Cast<ACharacter>(GetOwner());
-
+    CachedOwningASC = GetOwnerASC();
     UOWFireDataAsset* CurrentFireData = GetFireData();
+    
     if (CurrentFireData)
     {
         BurstShotsRemaining = CurrentFireData->FirePattern.FullBullet;
@@ -39,6 +44,20 @@ UOWFireDataAsset* UOWWeaponComponent::GetFireData()
     return FireData;
 }
 
+// WeaponComponent ê°™ì€ ìºë¦­í„° ì¢…ì† í´ë˜ìŠ¤ ì•ˆì—ì„œ
+UAbilitySystemComponent* UOWWeaponComponent::GetOwnerASC() const
+{
+    if (const ACharacter* Char = Cast<ACharacter>(GetOwner()))
+    {
+        if (AOWPlayerState* PS = Char->GetPlayerState<AOWPlayerState>())
+        {
+            return PS->GetAbilitySystemComponent();
+        }
+    }
+    return nullptr;
+}
+
+
 void UOWWeaponComponent::StartFire(const FVector Dir, const FGameplayEffectSpecHandle& InSpec)
 {
     CurrentDamageSpec = InSpec;
@@ -59,7 +78,67 @@ void UOWWeaponComponent::HandleFireTick()
         return;
     }
 
-    SpawnProjectile();
+    if (FireData->FirePattern.IsRayCast) {
+        ShootRay();
+    }
+    else {
+        SpawnProjectile();
+    }
+}
+
+void UOWWeaponComponent::ShootRay()
+{
+    if (!CachedOwningCharacter || !FireData) { return; }
+
+    FVector Start;
+    FRotator ViewRot;
+    CachedOwningCharacter->GetActorEyesViewPoint(Start, ViewRot);
+
+    FVector AdjustRotation =
+        (ViewRot.Vector() * Direction.X) +  // ì „ë°©
+        (FRotationMatrix(ViewRot).GetUnitAxis(EAxis::Y) * Direction.Y) +  // ì˜¤ë¥¸ìª½
+        (FRotationMatrix(ViewRot).GetUnitAxis(EAxis::Z) * Direction.Z);
+    AdjustRotation = AdjustRotation.GetSafeNormal();
+
+    const float RayLen = FireData->FirePattern.RayLength;
+    const FVector End = Start + AdjustRotation * RayLen;
+
+    FHitResult Hit;
+    FCollisionQueryParams Params(SCENE_QUERY_STAT(ShootRay), /*bTraceComplex*/false, CachedOwningCharacter);
+    Params.bReturnPhysicalMaterial = false;
+
+    const bool bHit = GetWorld()->LineTraceSingleByChannel(
+        Hit, Start, End, ECC_Visibility, Params);
+
+
+#if WITH_EDITOR
+    DrawDebugLine(GetWorld(), Start, End, bHit ? FColor::Red : FColor::Green, false, 2.f, 0, 1.f);
+#endif
+
+    AActor* Victim = Hit.GetActor();
+    if (!Victim) { return; }
+
+    if (UAbilitySystemComponent* VictimASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Victim))
+    {
+        if (FireData->FirePattern.DamageGameplayEffect)
+        {
+            FGameplayEffectContextHandle Ctx = VictimASC->MakeEffectContext();
+            Ctx.AddSourceObject(this);
+
+            FGameplayEffectSpecHandle Spec =
+                VictimASC->MakeOutgoingSpec(FireData->FirePattern.DamageGameplayEffect,
+                    /*Level*/1.f, Ctx);
+
+            if (Spec.IsValid())
+            {
+                const float Damage = FireData->FirePattern.Damage;
+                Spec.Data->SetSetByCallerMagnitude(
+                    OWGameplayTags::Data_Damage, Damage);
+
+                VictimASC->ApplyGameplayEffectSpecToSelf(*Spec.Data.Get());
+            }
+        }
+    }
 }
 
 void UOWWeaponComponent::SpawnProjectile()
@@ -69,8 +148,8 @@ void UOWWeaponComponent::SpawnProjectile()
     CachedOwningCharacter->GetActorEyesViewPoint(MuzzleLocation, MuzzleRotation);
 
     FVector AdjustRotation =
-        (MuzzleRotation.Vector() * Direction.X) +  // Àü¹æ
-        (FRotationMatrix(MuzzleRotation).GetUnitAxis(EAxis::Y) * Direction.Y) +  // ¿À¸¥ÂÊ
+        (MuzzleRotation.Vector() * Direction.X) +  // ì „ë°©
+        (FRotationMatrix(MuzzleRotation).GetUnitAxis(EAxis::Y) * Direction.Y) +  // ì˜¤ë¥¸ìª½
         (FRotationMatrix(MuzzleRotation).GetUnitAxis(EAxis::Z) * Direction.Z);
     MuzzleRotation = AdjustRotation.Rotation();
 
@@ -87,7 +166,7 @@ void UOWWeaponComponent::SpawnProjectile()
         //Projectile->DamageSpecHandle = CurrentDamageSpec;
         Projectile->OverlapEffectSpec.Add(CurrentDamageSpec);
 
-        Projectile->FinishSpawning(FTransform::Identity);  // ÀÓ½Ã Æ®·£½ºÆûÀ¸·Î ¿Ï·á
+        Projectile->FinishSpawning(FTransform::Identity);  // ì„ì‹œ íŠ¸ëœìŠ¤í¼ìœ¼ë¡œ ì™„ë£Œ
 
         Projectile->SetActorLocation(MuzzleLocation);
         Projectile->SetActorRotation(MuzzleRotation);
